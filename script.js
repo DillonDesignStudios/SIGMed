573ff175ddb967c6c59aae1572e907e202e5cad7
Chris Dillon <chris@dillondesignstudios.com>
2018-10-29 18:15:16 -0400

    Full site
    Mobile icon nav does not work

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    
+
+    
+   
+    $('.scrollContact').click(function(){
+        $('html, body').animate({scrollTop: $('.jsScrollContact').offset().top}, 2000);
+    });
+    $('.scrollTop').click(function(){
+        $('html, body').animate({scrollTop: $('.jsScrollTop').offset().top}, 2000);
+    });
+    $('.scrollNews').click(function(){
+        $('html, body').animate({scrollTop: $('.jsScrollNews').offset().top}, 2000);
+    });
+    $('.scrollRib').click(function(){
+        $('html, body').animate({scrollTop: $('.jsScrollRib').offset().top}, 2000);
+    });
+    $('.scrollVideo').click(function(){
+        $('html, body').animate({scrollTop: $('.jsScrollVideo').offset().top}, 2000);
+    });
+    $('.scrollAbout').click(function () {
+        $('html, body').animate({scrollTop: $('.jsScrollAbout').offset().top}, 2000);
+    });
+     $('.scrollEvents').click(function () {
+        $('html, body').animate({scrollTop: $('.jsScrollEvents').offset().top}, 2000);
+    });
+    
+});
